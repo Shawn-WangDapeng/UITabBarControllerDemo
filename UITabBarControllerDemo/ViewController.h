a462d343c26e0095dbb84e9ec4efc03eaf8b5e37
peng.wang.sh <peng.wang.sh@dianping.com>
2015-10-10 15:26:13 +0800

    Initial Commit

---
+++ UITabBarControllerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITabBarControllerDemo
+//
+//  Created by 王鹏 on 10/10/15.
+//  Copyright © 2015 dianping.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UITabBarControllerDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  UITabBarControllerDemo
+//
+//  Created by 王鹏 on 10/10/15.
+//  Copyright © 2015 dianping.com. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
